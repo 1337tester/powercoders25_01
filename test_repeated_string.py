9cbacb9985a26f4f0a066ca6e804c69b3d9354b6
Michal <miso.zima@1337tester.com>
2025-06-12 13:43:53 +0200

    first commit

---
+++ test_repeated_string.py
@@ -0,0 +1,7 @@
+from RepeatedString import repeated_string
+
+def test_repeated_string():
+    """Test the repeated_string function"""
+    
+    # Test cases
+    assert repeated_string("aba",10) == 7
