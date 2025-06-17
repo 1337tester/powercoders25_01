386d1b9730487ecece748aee6573e537d3ecb4ba
Michal <miso.zima@1337tester.com>
2025-06-17 14:40:29 +0200

    ping tests works

--- test_api.py
+++ test_api.py
@@ -1,21 +1,14 @@
 import requests
+import pytest
 
-def test_get_book_by_id():
-    url = "https://restful-booker.herokuapp.com/ping"  # Replace with your mock API URL
-    response = requests.get(url)
+@pytest.fixture
+def base_url():
+    return "https://restful-booker.herokuapp.com/"
+
+def test_ping(base_url):
+    response = requests.get(base_url + "ping")
 
     # Verify status code
     assert response.status_code == 201
     # Verify content-type
     assert response.headers["Content-Type"] == "text/plain; charset=utf-8"
-    
-
-  
-
-
-    # Verify response structure (Assuming a book object with 'id', 'title', and 'author')
-    #data = response.json()  
-    #assert isinstance(data, dict)
-    #assert "id" in data
-    #assert "title" in data
-    #assert "author" in data
