c120b972ee03db91a7d3ab93d34f60197d282b97
Michal <miso.zima@1337tester.com>
2025-06-17 10:48:41 +0200

    demonstrate CI CD

--- repeated_string.py
+++ repeated_string.py
@@ -1,6 +1,5 @@
 # Problem: https://www.hackerrank.com/challenges/repeated-string/problem
 # Score: 20
 
-
 def repeated_string(s, n):
     return n // len(s) * s.count('a') + s[0: n % len(s)].count('a')
