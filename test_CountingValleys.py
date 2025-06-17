aa1ce9cfbf963ea3d8b4fb625cf85231090d5b2b
abletcan <abulaiti.abulimiti@powercodrs.org>
2025-06-17 11:09:42 +0200

    :

--- test_CountingValleys.py
+++ test_CountingValleys.py
@@ -1,4 +1,6 @@
 from counting_valleys import counting_valleys
+import pytest
 
+@pytest.mark.counting
 def test_counting_valleys():
     assert counting_valleys(8,"UDDDUDUU") == 1 
--- test_jumpingClouds.py
+++ test_jumpingClouds.py
@@ -1,5 +1,7 @@
 
 from jumping_clouds import jumping_on_clouds
+import pytest
 
+@pytest.mark.jumping
 def test_jumping_clouds():
     assert jumping_on_clouds(7, [0, 0, 1, 0, 0, 1, 0]) == 4
